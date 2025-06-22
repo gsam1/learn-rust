1cad1942e1edd599887423750d6154a9ad075ce9
gsam1 <gsamardjiev@gmail.com>
2025-06-22 10:32:25 +0300

    Finished iterators 1 and 2

--- rustlings/exercises/18_iterators/iterators2.rs
+++ rustlings/exercises/18_iterators/iterators2.rs
@@ -7,7 +7,7 @@ fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
     match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_uppercase().to_string() + chars.as_str(),
     }
 }
 
@@ -15,14 +15,16 @@ fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+    let capital_words = words.iter().map(|x|capitalize_first(x)).collect();
+    capital_words
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+    let capital_words: Vec<String> = words.iter().map(|x|capitalize_first(x)).collect();
+    capital_words.join("")
 }
 
 fn main() {
