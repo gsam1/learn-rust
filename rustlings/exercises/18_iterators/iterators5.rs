5e56e379133351bda5c6b299d1aca3e96b808247
gsam1 <gsamardjiev@gmail.com>
2025-07-20 10:15:30 +0300

    Completed iterators

--- rustlings/exercises/18_iterators/iterators5.rs
+++ rustlings/exercises/18_iterators/iterators5.rs
@@ -28,6 +28,9 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // `map` is a hash map with `String` keys and `Progress` values.
     // map = { "variables1": Complete, "from_str": None, … }
+    let mut len: usize = 0;
+    let _: Vec<(&String, &Progress)> = map.into_iter().filter(|s| s.1 == &value).inspect(|_| len+= 1).collect();
+    len
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -48,6 +51,9 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // `collection` is a slice of hash maps.
     // collection = [{ "variables1": Complete, "from_str": None, … },
     //               { "variables2": Complete, … }, … ]
+    let mut count: usize = 0;
+    let _: Vec<&HashMap<String, Progress>> = collection.into_iter().inspect(|map| count += count_iterator(map, value)).collect();
+    count
 }
 
 fn main() {
