4ddd90bfe4960c8301c2899ce5ac519cde420b0c
gsam1 <gsamardjiev@gmail.com>
2025-07-27 14:52:17 +0300

    Solved smart pointers

--- rustlings/exercises/19_smart_pointers/arc1.rs
+++ rustlings/exercises/19_smart_pointers/arc1.rs
@@ -29,7 +29,7 @@ fn main() {
 
     for offset in 0..8 {
         // TODO: Define `child_numbers` using `shared_numbers`.
-        // let child_numbers = ???;
+        let child_numbers = Arc::new([1,2,3,4,5,6]);
 
         let handle = thread::spawn(move || {
             let sum: u32 = child_numbers.iter().filter(|&&n| n % 8 == offset).sum();
